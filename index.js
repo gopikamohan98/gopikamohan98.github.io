0ad90eed4cdaad20f2c3f427179a9c6343a4d4ad
gopumohan <gopikagopumohan98@gmail.com>
2022-08-09 21:03:12 +0530

    added projects

--- index.js
+++ index.js
@@ -6,21 +6,24 @@ function aboutClick(){
     var b = document.getElementById("skills");
     var c = document.getElementById("education");
     var d = document.getElementById("resume");
+    var e = document.getElementById("projects");
     a.style.display = "block";
     b.style.display = "none";
     c.style.display = "none";
     d.style.display = "none";
+    e.style.display = "none";
 
 
     var a = document.getElementById("navAbout");
     var b = document.getElementById("navSkill");
     var c = document.getElementById("navEdu");
     var d = document.getElementById("navResume");
-
+    var e = document.getElementById("navProjects");
     a.style.background="grey";
     b.style.background="none";
     c.style.background="none"; 
     d.style.background="none";
+    e.style.background="none";
 }
 function skillClick(){
     console.log("skill click 1st line")
@@ -28,20 +31,24 @@ function skillClick(){
     var b = document.getElementById("skills");
     var c = document.getElementById("education");
     var d = document.getElementById("resume");
+    var e = document.getElementById("projects");
     a.style.display = "none";
     b.style.display = "block";
     c.style.display = "none";
     d.style.display = "none";
+    e.style.display = "none";
 
     var a = document.getElementById("navAbout");
     var b = document.getElementById("navSkill");
     var c = document.getElementById("navEdu");
     var d = document.getElementById("navResume");
+    var e = document.getElementById("navProjects");
 
     a.style.background="none";
     b.style.background="grey";
     c.style.background="none";
     d.style.background="none";
+    e.style.background="none";
 }
 function resumeClick(){
     console.log("resume click 1st line")
@@ -49,20 +56,24 @@ function resumeClick(){
     var b = document.getElementById("skills");
     var c = document.getElementById("education");
     var d = document.getElementById("resume");
+    var e = document.getElementById("projects");
     a.style.display = "none";
     b.style.display = "none";
     c.style.display = "none";
     d.style.display = "block";
+    e.style.display = "none";
 
     var a = document.getElementById("navAbout");
     var b = document.getElementById("navSkill");
     var c = document.getElementById("navEdu");
     var d = document.getElementById("navResume");
+    var e = document.getElementById("navProjects");
 
     a.style.background="none";
     b.style.background="none";
     c.style.background="none";
     d.style.background="grey";
+    e.style.background="none";
 
 }
 function eduClick(){
@@ -71,19 +82,49 @@ function eduClick(){
     var b = document.getElementById("skills");
     var c = document.getElementById("education");
     var d = document.getElementById("resume");
+    var e = document.getElementById("projects");
     a.style.display = "none";
     b.style.display = "none";
     c.style.display = "block";
     d.style.display = "none";
+    e.style.display = "none";
 
     var a = document.getElementById("navAbout");
     var b = document.getElementById("navSkill");
     var c = document.getElementById("navEdu");
     var d = document.getElementById("navResume");
+    var e = document.getElementById("navProjects");
 
     a.style.background="none";
     b.style.background="none";
     c.style.background="grey";
     d.style.background="none";
+    e.style.background="none";
+
+}
+function projectsClick(){
+    console.log("pro click 1st line")
+    var a = document.getElementById("about");
+    var b = document.getElementById("skills");
+    var c = document.getElementById("education");
+    var d = document.getElementById("resume");
+    var e = document.getElementById("projects");
+    a.style.display = "none";
+    b.style.display = "none";
+    c.style.display = "none";
+    d.style.display = "none";
+    e.style.display = "block";
+
+    var a = document.getElementById("navAbout");
+    var b = document.getElementById("navSkill");
+    var c = document.getElementById("navEdu");
+    var d = document.getElementById("navResume");
+    var e = document.getElementById("navProjects");
+
+    a.style.background="none";
+    b.style.background="none";
+    c.style.background="none";
+    d.style.background="none";
+    e.style.background="grey";
 
 }
